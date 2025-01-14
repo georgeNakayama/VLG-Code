9d4cf50acd2c2db2b8358b3af0317f0d0be1b254
Jan Ackermann <jan.ackermann@stanford.edu>
2025-01-13 17:45:48 -0800

    Add torch vqa dataset

---
+++ src/data/datasets/vqa.py
@@ -0,0 +1,87 @@
+import json
+import os
+
+import cv2
+import torch
+import torch.utils.data
+
+class LLaVAInstruct(torch.utils.data.Dataset):  
+    def __init__(
+        self, 
+        data_file_path: str,
+        image_root_path: str,
+        ): 
+
+        self.data_file_path = data_file_path
+        self.image_root_path = image_root_path
+        #################################
+        self.datapoints = json.load(open(self.data_file_path, "r"))
+
+        self.gt_cached = {}
+        self.gt_caching = True
+
+    def __len__(self):
+        """Number of entries in the dataset"""
+        return len(self.datapoints)  
+    
+    def _parepare_image(self, image_path: str):
+        """Fetch the image for the given index"""
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        return image
+    
+    def _is_complete_datapoint(self, datapoint: dict):
+        assert "id" in datapoint
+        assert "image" in datapoint
+        assert "conversations" in datapoint
+    
+    def _is_correct_conversation(self, conversation: list[dict]):
+        assert len(conversation) % 2 == 0
+        for i, conversation_part in enumerate(conversation):
+            if i % 2 == 0:
+                assert conversation_part["from"] == "human"
+            else:
+                assert not conversation_part["from"] == "human"
+    
+    # added from maria 
+    def __getitem__(self, idx):
+        """Called when indexing: read the corresponding data. 
+        Does not support list indexing"""
+        
+        if torch.is_tensor(idx):  # allow indexing by tensors
+            idx = idx.tolist()
+        
+        datapoint = self.datapoints[idx]
+        self._is_complete_datapoint(datapoint)
+
+        data_name = datapoint["id"]
+        image_path = os.path.join(self.image_root_path, datapoint["image"])
+        image = self._parepare_image(image_path)
+        if data_name in self.gt_cached:
+            conversation = self.gt_cached[data_name]
+        else:
+            conversation = datapoint["conversations"]
+            self.gt_cached[data_name] = conversation
+            
+        # dialog = [{"role":"system","content":[{"type": "text", "text": system_prompt}]}]
+        dialog = []
+        # question = [{"role":"system","content":[{"type": "text", "text": system_prompt}]}]
+        question = []
+
+        self._is_correct_conversation(conversation)
+        for conversation_part in conversation[:-2]:
+            if conversation["from"] == "human":
+                dialog.append({"role": "user", "content": conversation_part["value"]})
+            else:
+                dialog.append({"role": "assistant", "content": conversation_part["value"]})
+
+        question.append({"role": "user", "content": conversation[-2]["value"]})
+        ground_truth = [conversation[-1]["value"]]
+
+        return (
+            image_path,
+            image,
+            dialog,
+            question,
+            ground_truth,
+        ) 
