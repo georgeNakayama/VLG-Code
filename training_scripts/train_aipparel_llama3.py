0925ab61582f2ae11667474bcff86784b524a124
ackjan <ackjan@n14.cm.cluster>
2025-02-12 14:29:14 -0800

    Refactor train script further

--- training_scripts/train_aipparel_llama3.py
+++ training_scripts/train_aipparel_llama3.py
@@ -44,7 +44,7 @@ def get_ddp_stats() -> tuple[int, int, int, bool]:
     master_process = (ddp_rank == 0)
     return ddp_rank, ddp_local_rank, ddp_world_size, master_process
 
-def add_tokens_to_garment_tokenizer(garment_tokenizer, processor):
+def add_tokens_to_garment_tokenizer(garment_tokenizer, processor) -> tuple[int, dict]:
     processor.tokenizer.pad_token = "<|finetune_right_pad_id|>"
     all_new_tokens = garment_tokenizer.get_all_token_names()
     num_added_tokens = processor.tokenizer.add_tokens(all_new_tokens, special_tokens=True)
@@ -53,7 +53,7 @@ def add_tokens_to_garment_tokenizer(garment_tokenizer, processor):
         token_idx = processor.tokenizer(token, add_special_tokens=False).input_ids[0]
         token_name2_idx_dict[token] = token_idx
     garment_tokenizer.set_token_indices(token_name2_idx_dict)
-    num_added_tokens, token_name2_idx_dict
+    return num_added_tokens, token_name2_idx_dict
 
 def get_model(cfg: MainConfig, ddp_local_rank) -> AIpparelMllavaNextForConditionalGeneration:
     torch_dtype = torch.float32
@@ -78,6 +78,8 @@ def get_model(cfg: MainConfig, ddp_local_rank) -> AIpparelMllavaNextForCondition
         p.requires_grad = False
     for p in model.multi_modal_projector.parameters():
         p.requires_grad = False
+    
+    return model
 
 def add_tokens_to_model_config(model: AIpparelMllavaNextForConditionalGeneration, garment_tokenizer) -> None:
     model.config.transf_token = PanelEdgeTypeV3.MOVE.value
@@ -108,6 +110,49 @@ def add_tokens_to_model_config(model: AIpparelMllavaNextForConditionalGeneration
     model.config.carc_token_index = garment_tokenizer.panel_edge_type_indices.closure_arc_idx
     
     model.config.zero_tensor = garment_tokenizer.get_zero_vertices()
+
+def get_deepspeed_config(cfg: MainConfig) -> dict:
+    optimizer_config = {
+        "type": "AdamW",
+        "params": {
+            "lr": cfg.optimizer.lr,
+            "weight_decay": 0.0,
+            "betas": (cfg.optimizer.beta1, cfg.optimizer.beta2)
+        }
+    }
+    scheduler_config = {
+        "type": "WarmupDecayLR",
+        "params": {
+            "total_num_steps": cfg.num_steps,
+            "warmup_min_lr": 0,
+            "warmup_max_lr": cfg.optimizer.lr,
+            "warmup_num_steps": cfg.warmup_steps,
+            "warmup_type": "linear"
+        }
+    }
+    ds_config = {
+        "train_micro_batch_size_per_gpu": cfg.batch_size,
+        "gradient_accumulation_steps": cfg.grad_accumulation_steps,
+        "fp16": {
+            "enabled": cfg.precision == "fp16",
+        },
+        "bf16": {
+            "enabled": cfg.precision == "bf16",
+        },
+        "gradient_clipping": 1.0,
+        "zero_optimization": {
+            "stage": 2,
+            "contiguous_gradients": True,
+            "overlap_comm": True,
+            "reduce_scatter": True,
+            "reduce_bucket_size": 5e8,
+            "allgather_bucket_size": 5e8,
+        },
+
+    }
+    ds_config["optimizer"] = optimizer_config
+    ds_config["scheduler"] = scheduler_config
+    return ds_config
 
 
 @hydra.main(version_base=None, config_path='../configs', config_name='config')
@@ -147,7 +192,7 @@ def main(cfg: MainConfig):
         log.info(f"Added {num_added_tokens} tokens to the tokenizer.")
         log.info(f"Token name to index dictionary: {token_name2_idx_dict}")
        
-    model = get_model(cfg, garment_tokenizer, ddp_local_rank)
+    model = get_model(cfg, ddp_local_rank)
     add_tokens_to_model_config(model, garment_tokenizer)
     model.resize_token_embeddings(len(processor.tokenizer) - model.vocab_size)
     total_params = sum(p.numel() for p in model.parameters())
@@ -155,130 +200,8 @@ def main(cfg: MainConfig):
     if master_process:
         log.info(f"Total parameters: {total_params}, Trainable parameters: {trainable_params} ({trainable_params/total_params:.4f})")
 
-    if cfg.eval_only:
-        init_process_group(backend="nccl")
-        torch.cuda.set_device(ddp_local_rank)
-        sampler = torch.utils.data.distributed.DistributedSampler(
-            dataset_val, shuffle=False, drop_last=False
-        )
-        val_loader = DataLoader(
-            dataset_val,
-            batch_size=1,
-            shuffle=False,
-            num_workers=12,
-            pin_memory=False,
-            sampler=sampler,
-            collate_fn=partial(
-                collate_fn,
-                processor=processor,
-                garment_tokenizer=garment_tokenizer,
-                model_version=cfg.version,
-                inference=False
-            ),
-        )
-        evaluate(
-            cfg,
-            model,
-            val_loader,
-            garment_tokenizer,
-            ddp_local_rank, 
-            ddp_world_size
-        )
-    elif cfg.gen_only:
-        init_process_group(backend="nccl")
-        torch.cuda.set_device(ddp_local_rank)
-        if cfg.gen_split == "train":
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                dataset_train, shuffle=False, drop_last=False
-            )
-            loader = DataLoader(
-                dataset_train,
-                batch_size=1,
-                shuffle=False,
-                num_workers=12,
-                pin_memory=False,
-                sampler=sampler,
-                collate_fn=partial(
-                    collate_fn,
-                    processor=processor,
-                    garment_tokenizer=garment_tokenizer,
-                    model_version=cfg.version
-                ),
-            )
-            gen_num = 100
-        elif cfg.gen_split == "val":
-            sampler = torch.utils.data.distributed.DistributedSampler(
-                dataset_val, shuffle=False, drop_last=False
-            )
-            loader = DataLoader(
-                dataset_val,
-                batch_size=1,
-                shuffle=False,
-                num_workers=12,
-                pin_memory=False,
-                sampler=sampler,
-                collate_fn=partial(
-                    collate_fn,
-                    processor=processor,
-                    garment_tokenizer=garment_tokenizer,
-                    model_version=cfg.version,
-                    inference=True
-                ),
-            )
-            gen_num = -1
-        os.makedirs(os.path.join(output_dir, f"generation_outputs_{cfg.gen_split}"), exist_ok=True)
-        generate(
-            cfg,
-            model, 
-            loader, 
-            garment_tokenizer,
-            processor,
-            ddp_rank, 
-            ddp_world_size,
-            os.path.join(output_dir, f"generation_outputs_{cfg.gen_split}"),
-            gen_num
-        )
-    else:
-        optimizer_config = {
-            "type": "AdamW",
-            "params": {
-                "lr": cfg.optimizer.lr,
-                "weight_decay": 0.0,
-                "betas": (cfg.optimizer.beta1, cfg.optimizer.beta2)
-            }
-        }
-        scheduler_config = {
-            "type": "WarmupDecayLR",
-            "params": {
-                "total_num_steps": cfg.num_steps,
-                "warmup_min_lr": 0,
-                "warmup_max_lr": cfg.optimizer.lr,
-                "warmup_num_steps": cfg.warmup_steps,
-                "warmup_type": "linear"
-            }
-        }
-        ds_config = {
-            "train_micro_batch_size_per_gpu": cfg.batch_size,
-            "gradient_accumulation_steps": cfg.grad_accumulation_steps,
-            "fp16": {
-                "enabled": cfg.precision == "fp16",
-            },
-            "bf16": {
-                "enabled": cfg.precision == "bf16",
-            },
-            "gradient_clipping": 1.0,
-            "zero_optimization": {
-                "stage": 2,
-                "contiguous_gradients": True,
-                "overlap_comm": True,
-                "reduce_scatter": True,
-                "reduce_bucket_size": 5e8,
-                "allgather_bucket_size": 5e8,
-            },
-
-        }
-        ds_config["optimizer"] = optimizer_config
-        ds_config["scheduler"] = scheduler_config
+    if not cfg.eval_only or cfg.gen_only:
+        ds_config = get_deepspeed_config(cfg)
         model_engine, optimizer, train_loader, scheduler = deepspeed.initialize(
             model=model,
             model_parameters=model.parameters(),
@@ -303,8 +226,49 @@ def main(cfg: MainConfig):
             ddp_world_size,
             output_dir
         )
-    destroy_process_group("nccl")
+    else:
+        assert not (cfg.eval_only and cfg.gen_only), "Only eval or gen can be set at a time!"
+        assert not cfg.eval_only or cfg.gen_split == "val", "Only perform eval on the val dataset!"
 
+        init_process_group(backend="nccl")
+        torch.cuda.set_device(ddp_local_rank)
+        dataset = dataset_val if cfg.gen_split == "val" else dataset_train
+        sampler = torch.utils.data.distributed.DistributedSampler(
+            dataset, shuffle=False, drop_last=False
+        )
+        data_loader = DataLoader(
+            dataset_val,
+            batch_size=1,
+            shuffle=False,
+            num_workers=12,
+            pin_memory=False,
+            sampler=sampler,
+            collate_fn=partial(
+                collate_fn,
+                processor=processor,
+                garment_tokenizer=garment_tokenizer,
+                model_version=cfg.version,
+                inference=cfg.eval_only
+            ),
+        )
+        if cfg.eval_only:
+            evaluate(cfg, model, data_loader, garment_tokenizer, ddp_local_rank, ddp_world_size)
+        else:
+            gen_num = 100 if cfg.gen_split == "train" else -1
+            generation_output_dir = os.path.join(output_dir, f"generation_outputs_{cfg.gen_split}")
+            os.makedirs(generation_output_dir, exist_ok=True)
+            generate(
+                cfg,
+                model, 
+                data_loader, 
+                garment_tokenizer,
+                processor,
+                ddp_rank, 
+                ddp_world_size,
+                generation_output_dir,
+                gen_num
+            )
+        destroy_process_group()
 
 if __name__ == "__main__":
     main()
